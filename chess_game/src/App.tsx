25858c1fed66ed40d98a71b700d59df6bff21407
Baudouin Flament <baudouinflament@gmail.com>
2022-08-29 17:41:57 +0200

    chess.js

--- chess_game/src/App.tsx
+++ chess_game/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import Chessboard from "chessboardjsx";
+import { useState } from 'react';
 import './App.css';
+import Chessboard from "chessboardjsx";
+import { ChessInstance } from "chess.js";
+const Chess = require("chess.js");
 
 const App: React.FC = () => {
+
+  const [chess] = useState<ChessInstance>(
+    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+  );
+
   return (
     <div className="flex-center">
       <h1>Random Chess</h1>
